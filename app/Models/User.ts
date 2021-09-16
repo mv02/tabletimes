2b1f947fee7702c92c82a7b55866d8293249f351
Milan <milanvodak02@gmail.com>
2021-09-16 18:33:25 +0200

    Add password field to users

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -10,6 +10,9 @@ export default class User extends BaseModel {
   @column()
   public email: string;
 
+  @column({ serializeAs: null })
+  public password: string | null;
+
   @column()
   public firstName: string | null;
 
